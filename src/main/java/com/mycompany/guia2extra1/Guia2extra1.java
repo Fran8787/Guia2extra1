46840418b308c40896cf36fb62232acf5b00a40b
Fran8787 <josefranciscoandrian@gmail.com>
2023-08-18 15:56:13 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/guia2extra1/Guia2extra1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guia2extra1;
+
+/**
+ *
+ * @author jfa_j
+ */
+public class Guia2extra1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
